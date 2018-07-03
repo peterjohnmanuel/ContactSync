8e387a60b3b73520dc9a3a9c73b8a08b6c4e5628
Peter <peterjohn.manuel@gmail.com>
2018-07-03 21:32:57 +0200

    Phone book and entry entities added to ContactSync.Entities project.

---
+++ ContactSyncService/ContactSync.Entities/ContactSync.Entities/Entry.cs
@@ -0,0 +1,9 @@
+﻿namespace ContactSync.Entities
+{
+    public class Entry
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public string PhoneNumber { get; set; }
+    }
+}
---
+++ ContactSyncService/ContactSync.Entities/ContactSync.Entities/PhoneBook.cs
@@ -0,0 +1,8 @@
+﻿namespace ContactSync.Entities
+{
+    public class PhoneBook
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+    }
+}
