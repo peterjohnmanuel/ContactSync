8e06cb74aea3aaf9600baa274d79831507574ba1
Peter <peterjohn.manuel@gmail.com>
2018-07-03 21:45:01 +0200

    Context for database interaction added with project ContactSync.Context class library project.

---
+++ ContactSyncService/ContactSync.Entities/ContactSync.Context/ContactSyncContext.cs
@@ -0,0 +1,15 @@
+﻿using ContactSync.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace ContactSync.Context
+{
+    public class ContactSyncContext : DbContext
+    {
+        public DbSet<PhoneBook> PhoneBooks { get; set; }
+        public DbSet<Entry> Entries { get; set; }
+
+        public ContactSyncContext(DbContextOptions<ContactSyncContext> options) : base(options)
+        {
+        }
+    }
+}
