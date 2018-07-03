4bfc02e17905a7342d5defbca445613ea6eca76c
Peter <peterjohn.manuel@gmail.com>
2018-07-03 22:20:04 +0200

    Business logic project added for implementation of interface class project.

---
+++ ContactSyncService/ContactSync.Entities/ContactSync.BusinessLogic/PhoneBookBusinessLogic.cs
@@ -0,0 +1,22 @@
+﻿using ContactSync.Entities;
+using ContactSync.IBusinessLogic;
+using ContactSync.IRepository;
+using System.Collections.Generic;
+
+namespace ContactSync.BusinessLogic
+{
+    public class PhoneBookBusinessLogic : IPhoneBookBusinessLogic
+    {
+        private readonly IPhoneBookRepository phoneBookRepository;
+
+        public PhoneBookBusinessLogic(IPhoneBookRepository phoneBookRepository)
+        {
+            this.phoneBookRepository = phoneBookRepository;
+        }
+
+        public IEnumerable<PhoneBook> GetAllPhoneBooks()
+        {
+            return phoneBookRepository.GetAllPhoneBooks();
+        }
+    }
+}
